0632d97bcfba61cc4c1720925a7a5f48978ec1e0
Pebertli Barata <pebertli@gmail.com>
2024-01-17 12:13:04 +0000

    Update image processing output type to 'pil'

--- src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint_sd_xl.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint_sd_xl.py
@@ -1682,10 +1682,13 @@ class StableDiffusionXLControlNetInpaintPipeline(
         if self.watermark is not None:
             image = self.watermark.apply_watermark(image)
 
-        image = self.image_processor.postprocess(image, output_type=output_type)
+        image = self.image_processor.postprocess(image, output_type='pil')
 
         # Offload all models
         self.maybe_free_model_hooks()
+
+        if output_type == "both":
+            return (image, latents)
 
         if not return_dict:
             return (image,)
